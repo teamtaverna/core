40af0d1ac6fcda8d85567d35ff3f1f2f869fb926
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-11 11:32:50 +0100

    Remove unneccessary date parameter

--- app/shared/models.py
+++ app/shared/models.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 class CommonInfo(models.Model):
-    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True

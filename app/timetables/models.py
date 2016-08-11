c7cf6587fc24d7fa11a963112e5b1826fbe76a9b
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-11 22:04:13 +0100

    Create Course model

--- app/timetables/models.py
+++ app/timetables/models.py
@@ -1,3 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from app.shared.models import CommonInfo
+
+class Course(CommonInfo, models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str(self):
+        return self.name

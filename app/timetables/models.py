5b88169dfcaee03d21805768063aa943fcc744e7
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-11 23:19:38 +0100

    Add course model to admin

--- app/timetables/models.py
+++ app/timetables/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
 from app.shared.models import CommonInfo
 
-class Course(CommonInfo, models.Model):
-    name = models.CharField(max_length=150)
+
+class Course(CommonInfo):
+    name = models.CharField(max_length=150, unique=True)
 
     def __str(self):
         return self.name

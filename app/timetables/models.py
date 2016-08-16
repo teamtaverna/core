ac3e42d9226e28d84ced0689acb6a8fb6b5cb71f
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-08-16 23:25:04 +0100

    Code cleanup, add tests for meal model

--- app/timetables/models.py
+++ app/timetables/models.py
@@ -2,28 +2,33 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from common.mixins import ForceCapitalizeMixin
 
-class Weekday(models.Model):
+
+class Weekday(ForceCapitalizeMixin, models.Model):
     """Model representing the day of the week."""
 
     name = models.CharField(max_length=60, unique=True)
 
-    def clean(self):
-        """
-        Capitalize the first letter of the first word to avoid case
-        insensitive duplicates for name field.
-        """
-        self.name = self.name.capitalize()
+    capitalized_field_names = ('name',)
 
-    def save(self, *args, **kwargs):
-        self.clean()
-        return super(Weekday, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
 
 
-class Meal(models.Model):
+class Meal(ForceCapitalizeMixin, models.Model):
+    """
+    Model representing food occasions.
+
+    This represents an occasion during the day that food
+    is scheduled to be served. E.g breakfast, lunch, etc.
+    """
+
     name = models.CharField(max_length=60, unique=True)
     start_time = models.TimeField()
     end_time = models.TimeField()
+
+    capitalized_field_names = ('name',)
 
     def __str__(self):
         return self.name
--- app/timetables/tests/test_models.py
+++ app/timetables/tests/test_models.py
@@ -1,21 +1,40 @@
 from django.test import TestCase
 from django.db import IntegrityError
 
-from app.timetables.models import Weekday
+from app.timetables.models import Weekday, Meal
 
 
 class WeekdayTest(TestCase):
     """Tests the Weekday model."""
 
     def setUp(self):
-        Weekday.objects.create(name="monday")
+        Weekday.objects.create(name='monday')
 
     def test_weekday_name_should_be_capitalized_on_save(self):
-        day = Weekday.objects.get(name__iexact="monday")
+        day = Weekday.objects.get(name__iexact='monday')
 
-        self.assertEqual(day.name, "Monday")
+        self.assertEqual(day.name, 'Monday')
 
     def test_duplicate_weekday_name_cannot_be_saved(self):
-        day = Weekday(name="Monday")
+        day = Weekday(name='Monday')
 
         self.assertRaises(IntegrityError, day.save)
+
+
+class MealTest(TestCase):
+    """Tests the Meal model."""
+
+    def setUp(self):
+        Meal.objects.create(
+            name='breakfast', start_time='21:30:05', end_time='22:30:05'
+        )
+
+    def test_meal_name_should_be_capitalized_on_save(self):
+        meal = Meal.objects.get(name__iexact="breakfast")
+
+        self.assertEqual(meal.name, 'Breakfast')
+
+    def test_duplicate_meal_name_cannot_be_saved(self):
+        meal = Meal(name='Breakfast')
+
+        self.assertRaises(IntegrityError, meal.save)

9b02a09be67c8ec3d3b4b652d98f2cd5c3fdc863
andela-kerinoso <kolawole.erinoso@andela.com>
2016-09-13 23:29:59 +0100

    Add Timetables Admin model to Django Admin Interface

--- app/timetables/admin.py
+++ app/timetables/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Meal, MealOption, Weekday, Timetable, Dish
-
+from .models import *
 
 admin.site.register(Weekday)
 admin.site.register(Meal)
@@ -9,3 +8,4 @@ admin.site.register(MealOption)
 admin.site.register(Course)
 admin.site.register(Timetable)
 admin.site.register(Dish)
+admin.site.register(Admin)

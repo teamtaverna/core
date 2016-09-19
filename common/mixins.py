7ce8f2628493a6d2dac9a2861038450990bd16c6
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-09-19 12:49:27 +0100

    Remove redundant ForceCapitalizeMixin

--- common/mixins.py
+++ common/mixins.py
@@ -35,18 +35,3 @@ class SlugifyMixin():
     def save(self, *args, **kwargs):
         self.clean()
         return super().save(*args, **kwargs)
-
-
-class ForceCapitalizeMixin():
-    """Capitalize the first letter of the model fields specified."""
-
-    def clean(self):
-        if hasattr(self, 'capitalized_field_names'):
-            for field_name in self.capitalized_field_names:
-                self.__dict__[field_name] = self.__dict__[field_name].capitalize()
-                # ToDO: Do a check to ensure that only capitalizable fields
-                # (TextField and CharField) are capitalized.
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        return super().save(*args, **kwargs)

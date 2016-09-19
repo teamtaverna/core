97b33e243c4aa36f8b3257a5c4f3388a935d9e9a
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-09-19 12:47:28 +0100

    Register model admins and specify order of fields arrangement

--- app/timetables/admin.py
+++ app/timetables/admin.py
@@ -3,15 +3,34 @@ from django.contrib import admin
 from . import models
 
 
-class WeekdayAdmin(admin.ModelAdmin):
+@admin.register(models.Weekday, models.MealOption, models.Course)
+class DefaultAdmin(admin.ModelAdmin):
+    """Default admin for models with just name and slug fields."""
     readonly_fields = ('slug',)
-    empty_value_display = ''
 
-admin.site.register(models.Weekday, WeekdayAdmin)
-admin.site.register(models.Meal)
-admin.site.register(models.MealOption)
-admin.site.register(models.Course)
-admin.site.register(models.Timetable)
-admin.site.register(models.Dish)
+
+@admin.register(models.Meal)
+class MealAdmin(DefaultAdmin):
+    fields = ('name', 'slug', 'start_time', 'end_time')
+
+
+class AdminsInline(admin.TabularInline):
+    model = models.Timetable.admins.through
+
+
+@admin.register(models.Timetable)
+class TimetableAdmin(DefaultAdmin):
+    fields = ('name', 'slug', 'code', 'api_key', 'cycle_length',
+              'current_cycle_day', 'description')
+    inlines = (AdminsInline,)
+
+
+@admin.register(models.Dish)
+class DishAdmin(DefaultAdmin):
+    fields = ('name', 'slug', 'description')
+
+
+admin.site.empty_value_display = ''
+
 admin.site.register(models.Admin)
 admin.site.register(models.MenuItem)

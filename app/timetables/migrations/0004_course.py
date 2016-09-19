0f1f7963c2ea80604593644e1c04643031561970
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-09-19 12:48:18 +0100

    Remove reference to ForceCapitalizeMixin from migration file and update with SlugifyMixin

--- app/timetables/migrations/0004_course.py
+++ app/timetables/migrations/0004_course.py
@@ -19,6 +19,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=150, unique=True)),
             ],
-            bases=(common.mixins.ForceCapitalizeMixin, models.Model),
+            bases=(common.mixins.SlugifyMixin, models.Model),
         ),
     ]

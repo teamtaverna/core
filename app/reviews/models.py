7c78812027863a922b16cc410a3c8438eecb7f17
andela-kerinoso <kolawole.erinoso@andela.com>
2016-10-18 12:28:21 +0100

    31 Create Review model

--- app/reviews/models.py
+++ app/reviews/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
 
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from common.mixins import TimestampMixin
+from app.timetables.models import Serving
+
+
+class Review(TimestampMixin):
+    """
+    Model representing ratings and comments
+    submitted by users for menu item
+    they had on a particular day of the cycle.
+    """
+
+    RATINGS = (
+        (5, 'EXCELLENT'),
+        (4, 'GOOD'),
+        (3, 'FAIR'),
+        (2, 'POOR'),
+        (1, 'TERRIBLE')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    serving = models.ForeignKey(Serving, on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(choices=RATINGS)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return '{}\'s review of {}'.format(self.user, self.serving)
+
+    class Meta:
+        unique_together = ('user', 'serving')

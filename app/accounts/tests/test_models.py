aede14a79f495f5a7fec89d77fdaf96c54dfe335
Jubril Issa <issa.jubril@andela.com>
2016-10-21 12:48:21 +0100

    Refactor to use factory boy

--- app/accounts/tests/test_models.py
+++ app/accounts/tests/test_models.py
@@ -1,15 +1,15 @@
-from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.test import TestCase
 
 from app.accounts.models import UserProfile
+from app.timetables.factories import UserFactory
 
 
 class UserProfileTest(TestCase):
     """Test UserProfile model"""
 
     def setUp(self):
-        self.user = User.objects.create(username='frank', password='secret')
+        self.user = UserFactory()
 
     def test_user_can_only_have_one_profile_entry(self):
         profile = UserProfile(user=self.user, custom_auth_id='134664323567')

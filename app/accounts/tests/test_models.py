39c4d50b08f92a5d76ac5864e13a3427e7dfd86a
Jubril Issa <issa.jubril@andela.com>
2016-10-21 10:11:16 +0100

    Add test for creating profile on user creation

--- app/accounts/tests/test_models.py
+++ app/accounts/tests/test_models.py
@@ -10,9 +10,11 @@ class UserProfileTest(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username='frank', password='secret')
-        UserProfile.objects.create(user=self.user)
 
     def test_user_can_only_have_one_profile_entry(self):
         profile = UserProfile(user=self.user, custom_auth_id='134664323567')
-
         self.assertRaises(IntegrityError, profile.save)
+
+    def test_profile_is_created_on_user_creation(self):
+        user_profile = UserProfile.objects.get(user=self.user)
+        self.assertEqual(user_profile.user, self.user)

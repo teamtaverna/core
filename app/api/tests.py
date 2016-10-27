d999c82228f3db9e117251ea90990f1bfc816a40
andela-kerinoso <kolawole.erinoso@andela.com>
2016-10-27 20:54:31 +0100

    133 Test that the api endpoint is live

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class ApiTest(TestCase):
+    """Test for API"""
+
+    def setUp(self):
+        self.client = Client()
+        self.endpoint = '/api'
+
+    def test_api_endpoint_is_live(self):
+        r = self.client.get(self.endpoint, {'query': ''})
+
+        self.assertEqual(400, r.status_code)
+        self.assertEqual('Must provide query string.', r.json()['errors'][0]['message'])

10bf401fd2e373b5d833e8234fcc2c99da7418f6
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-01-11 11:07:13 +0100

    update createweekday class

--- app/api/schema.py
+++ app/api/schema.py
@@ -3,11 +3,14 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .cruds.user_crud import UserNode, CreateUser, UpdateUser, DeleteUser
 from .cruds.dish_crud import DishNode, CreateDish, UpdateDish, DeleteDish
+from .cruds.weekday_crud import WeekdayNode, CreateWeekday
 
 
 class Query(graphene.AbstractType):
     user = graphene.relay.Node.Field(UserNode)
     users = DjangoFilterConnectionField(UserNode)
+    weekday = graphene.relay.Node.Field(WeekdayNode)
+    weekdays = DjangoFilterConnectionField(WeekdayNode)
 
     dish = graphene.relay.Node.Field(DishNode)
     dishes = DjangoFilterConnectionField(DishNode)
@@ -18,6 +21,9 @@ class Mutation(graphene.ObjectType):
     update_user = UpdateUser.Field()
     delete_user = DeleteUser.Field()
 
+
     create_dish = CreateDish.Field()
     update_dish = UpdateDish.Field()
     delete_dish = DeleteDish.Field()
+
+    create_Weekday = CreateWeekday.Field()

45b26dac2c7d54baa5aac381d5caedcabddacf77
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-01-12 10:42:15 +0100

    add update and delete weekday classes

--- app/api/cruds/weekday_crud.py
+++ app/api/cruds/weekday_crud.py
@@ -1,10 +1,10 @@
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist
 
 import graphene
 from graphene_django import DjangoObjectType
+from graphql_relay.node.node import from_global_id
 
 from app.timetables.models import Weekday
-from .utils import get_errors
 
 
 class WeekdayNode(DjangoObjectType):
@@ -23,20 +23,49 @@ class WeekdayNode(DjangoObjectType):
 
 
 class CreateWeekday(graphene.relay.ClientIDMutation):
+    weekday = graphene.Field(WeekdayNode)
+    errors = graphene.List(graphene.String)
 
     class Input:
         name = graphene.String(required=True)
 
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        weekday = Weekday(
+            input.get('name')
+        )
+        weekday.save()
+        return CreateWeekday(weekday=weekday)
+
+
+class UpdateWeekday(graphene.relay.ClientIDMutation):
     weekday = graphene.Field(WeekdayNode)
     errors = graphene.List(graphene.String)
+
+    class Input:
+        id = graphene.String(required=True)
+        name = graphene.String(required=True)
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        weekday = Weekday.objects.get(pk=from_global_id(input.get('id'))[1])
+        weekday.name = input.get('name')
+        weekday.save()
+        return UpdateWeekday(weekday=weekday)
+
+
+class DeleteWeekday(graphene.relay.ClientIDMutation):
+    weekday = graphene.Field(WeekdayNode)
+    deleted = graphene.Boolean()
+
+    class Input:
+        id = graphene.String(required=True)
 
     @classmethod
     def mutate_and_get_payload(cls, input, context, info):
         try:
-            weekday = Weekday()
-            weekday.name = input.get('name')
-            weekday.full_clean()
-            weekday.save()
-            return CreateWeekday(weekday=weekday)
-        except ValidationError as e:
-            return CreateWeekday(weekday=None, errors=get_errors(e))
+            weekday = Weekday.objects.get(pk=from_global_id(input.get('id'))[1])
+            weekday.delete()
+            return DeleteWeekday(deleted=True, weekday=weekday)
+        except ObjectDoesNotExist:
+            return DeleteWeekday(deleted=False, weekday=None)
--- app/api/schema.py
+++ app/api/schema.py
@@ -3,12 +3,13 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .cruds.user_crud import UserNode, CreateUser, UpdateUser, DeleteUser
 from .cruds.dish_crud import DishNode, CreateDish, UpdateDish, DeleteDish
-from .cruds.weekday_crud import WeekdayNode, CreateWeekday
+from .cruds.weekday_crud import WeekdayNode, CreateWeekday, UpdateWeekday, DeleteWeekday
 
 
 class Query(graphene.AbstractType):
     user = graphene.relay.Node.Field(UserNode)
     users = DjangoFilterConnectionField(UserNode)
+
     weekday = graphene.relay.Node.Field(WeekdayNode)
     weekdays = DjangoFilterConnectionField(WeekdayNode)
 
@@ -26,4 +27,6 @@ class Mutation(graphene.ObjectType):
     update_dish = UpdateDish.Field()
     delete_dish = DeleteDish.Field()
 
-    create_Weekday = CreateWeekday.Field()
+    create_weekday = CreateWeekday.Field()
+    update_weekday = UpdateWeekday.Field()
+    delete_weekday = DeleteWeekday.Field()

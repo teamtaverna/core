54765daf8ebe796dda0d615d2e6347f89b77eb28
andela-kerinoso <kolawole.erinoso@andela.com>
2017-01-20 15:19:56 +0100

    148 Wrap graphql api requests handlers with auth and csrf_exempt decorators

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
+from app.api.auth import authorization_required
 
 urlpatterns = [
     url(r'^api/', include('app.api.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api', GraphQLView.as_view(graphiql=True)),
+    url(r'^api$', csrf_exempt(authorization_required(GraphQLView.as_view(graphiql=True)))),
 ]

b12767d1b8ebab138537937ab7764f817992e0d7
andela-kerinoso <kolawole.erinoso@andela.com>
2017-01-20 15:08:51 +0100

    148 Map endpoints to views for issuing and revoking api keys

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 from graphene_django.views import GraphQLView
 
+
 urlpatterns = [
+    url(r'^api/', include('app.api.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api', GraphQLView.as_view(graphiql=True)),

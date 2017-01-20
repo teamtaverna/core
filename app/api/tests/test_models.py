0d001540ff9f8f26a02f982739d4f997eae8af9e
andela-kerinoso <kolawole.erinoso@andela.com>
2017-01-20 14:53:41 +0100

    148 Test ApiKey model

---
+++ app/api/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+from ..models import ApiKey
+
+class ApiKeyTest(TestCase):
+    """Tests the ApiKey model."""
+    
+    def test_only_superuser_can_create_api_key(self):
+        # Using normal users.
+        normal_user = User.objects.create_user('user1', 'user1@taverna.com', 'qwerty123')
+        self.assertRaises(ValidationError, ApiKey.objects.create, owner=normal_user)
+
+        # Using super users.
+        super_user = User.objects.create_superuser('admin', 'admin@taverna.com', 'qwerty123')
+        api_key = ApiKey.objects.create(owner=super_user)
+        self.assertIsInstance(api_key, ApiKey)

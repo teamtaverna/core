c1632c9e18aad3402c997804e0593f0abdad24b0
Sunday Nwuguru <sunday.nwuguru@andela.com>
2017-01-23 10:55:31 +0100

    136 Add test cases for dish endpoints

--- app/api/tests/test_dish.py
+++ app/api/tests/test_dish.py
@@ -11,6 +11,11 @@ class DishApiTest(TestCase):
             'name': 'rice',
             'description': 'white rice'
         }
+        self.dishes = (
+            ('rice', 'qwerty123'),
+            ('Coconut rice', 'qwerty123'),
+            ('plantain', 'qwerty123'),
+        )
 
     def create_dish(self, name, description):
         query = '''
@@ -27,8 +32,11 @@ class DishApiTest(TestCase):
 
         return self.client.post(self.endpoint, {'query': query}).json()
 
+    def create_multiple_dishes(self):
+        return [self.create_dish(name, description) for name, description in self.dishes]
+
     def retrieve_dish(self, id):
-        query = 'query {dish(id: "%s" ) {username}}' % (id)
+        query = 'query {dish(id: "%s" ) {name}}' % (id)
 
         return self.client.post(self.endpoint, data={'query': query}).json()
 
@@ -45,3 +53,181 @@ class DishApiTest(TestCase):
         }
 
         self.assertEqual(expected, response['data'])
+
+    def test_retrieval_of_one_dish_object(self):
+        expected = {
+            'dish': {
+                'name': self.data['name']
+            }
+        }
+        create_response = self.create_dish(self.data['name'], self.data['description'])
+        response = self.retrieve_dish(create_response['data']['createDish']['dish']['id'])
+
+        self.assertEqual(expected, response['data'])
+
+    def test_retrieval_of_multiple_dish_objects_without_filtering(self):
+        self.create_multiple_dishes()
+
+        query = 'query {dishes{edges{node{name}}}}'
+
+        expected = {
+            'dishes': {
+                'edges': [
+                  {
+                    'node': {
+                        'name': self.dishes[0][0]
+                    }
+                  },
+                  {
+                    'node': {
+                        'name': self.dishes[1][0]
+                    }
+                  },
+                  {
+                    'node': {
+                        'name': self.dishes[2][0]
+                    }
+                  }
+                ]
+            }
+        }
+
+        response = self.client.post(self.endpoint, {'query': query}).json()
+
+        self.assertEqual(expected, response['data'])
+
+    def test_retrieval_of_multiple_user_objects_filter_by_username(self):
+        self.create_multiple_dishes()
+        query = 'query {dishes(name_Icontains: "Rice") {edges{node{name}}}}'
+
+        expected = {
+            'dishes': {
+                'edges': [
+                  {
+                    'node': {
+                        'name': self.dishes[0][0]
+                    }
+                  },
+                  {
+                    'node': {
+                        'name': self.dishes[1][0]
+                    }
+                  }
+                ]
+            }
+        }
+
+        response = self.client.post(self.endpoint, {'query': query}).json()
+
+        self.assertEqual(expected, response['data'])
+
+    def test_update_of_dish_object(self):
+        create_response = self.create_dish(self.data['name'], self.data['description'])
+        query = '''
+            mutation{
+                updateDish(
+                    input: {
+                        id: "%s",
+                        name: "rice edited"
+                    }
+                )
+                {
+                    dish{
+                        id,
+                        name
+                    }
+                }
+            }
+        ''' % (create_response['data']['createDish']['dish']['id'])
+
+        expected = {
+            "updateDish": {
+                "dish": {
+                    "id": create_response['data']['createDish']['dish']['id'],
+                    "name": "rice edited"
+                }
+            }
+        }
+
+        response = self.client.post(self.endpoint, {'query': query}).json()
+
+        self.assertEqual(expected, response['data'])
+
+    def test_deletion_of_dish_object(self):
+        create_response = self.create_dish(self.data['name'], self.data['description'])
+        query = '''
+            mutation{
+                deleteDish(input: {id: "%s"}){
+                    dish{
+                        name
+                    }
+                }
+            }
+        ''' % (create_response['data']['createDish']['dish']['id'])
+
+        expected = {
+            "deleteDish": {
+                "dish": {
+                    "name": self.data['name']
+                }
+            }
+        }
+
+        response = self.client.post(self.endpoint, {'query': query}).json()
+        self.assertEqual(expected, response['data'])
+
+        response = self.retrieve_dish(create_response['data']['createDish']['dish']['id'])
+        self.assertEqual(None, response['data']['dish'])
+
+    def ordering_test_helper(self, ordering_param, records):
+        # For ascending ordering
+        query = 'query{dishes(orderBy: "%s") {edges{node{name}}}}' % (ordering_param)
+        expected = {
+            'dishes': {
+                'edges': [
+                  {
+                    'node': {
+                        'name': records[0]
+                    }
+                  },
+                  {
+                    'node': {
+                        'name': records[1]
+                    }
+                  },
+                  {
+                    'node': {
+                        'name': records[2]
+                    }
+                  }
+                ]
+            }
+        }
+        response = self.client.post(self.endpoint, {'query': query}).json()
+        self.assertEqual(expected, response['data'])
+
+        # For descending ordering
+        query = 'query {dishes(orderBy: "-%s") {edges{node{name}}}}' % (ordering_param)
+        expected['dishes']['edges'].reverse()
+        response = self.client.post(self.endpoint, {'query': query}).json()
+        self.assertEqual(expected, response['data'])
+
+    def test_retrieval_of_multiple_dish_objects_ordering_by_id(self):
+        self.create_multiple_dishes()
+        records = [
+            self.dishes[0][0],
+            self.dishes[1][0],
+            self.dishes[2][0]
+        ]
+
+        self.ordering_test_helper('id', records)
+
+    def test_retrieval_of_multiple_dish_objects_ordering_by_name(self):
+        self.create_multiple_dishes()
+        records = [
+            self.dishes[1][0],
+            self.dishes[2][0],
+            self.dishes[0][0]
+        ]
+
+        self.ordering_test_helper('name', records)

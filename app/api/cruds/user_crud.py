36b36a08db08dee74bdabd12056e35c365f63114
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-03-11 10:42:56 +0100

    Remove unnecessary import statements

--- app/api/cruds/user_crud.py
+++ app/api/cruds/user_crud.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.core.exceptions import ObjectDoesNotExist
 
 import graphene
 from graphene_django import DjangoObjectType
--- app/api/cruds/weekday_crud.py
+++ app/api/cruds/weekday_crud.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.core.exceptions import ValidationError
 
 import graphene
 from graphene_django import DjangoObjectType

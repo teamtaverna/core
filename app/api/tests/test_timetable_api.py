88e4d18edc337b319d1c767452859782f4e30f64
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-04-25 14:00:50 +0300

    Rebase and refactor

--- app/api/tests/test_timetable_api.py
+++ app/api/tests/test_timetable_api.py
@@ -1,27 +1,13 @@
 from django.test import Client, TestCase
 
-from .utils import obtain_api_key, create_admin_account
+from .utils import create_admin_account
 
 
 class TimetableApiTest(TestCase):
 
     def setup(self):
         self.client = Client()
-        self.endpoint = '/api'
-        self.admin_test_credentials = ('admin', 'admin@taverna.com', 'qwerty123')
-        create_admin_account(*self.admin_test_credentials)
-        self.header = {
-            'HTTP_X_TAVERNATOKEN': obtain_api_key(
-                self.client, *self.admin_test_credentials
-            )
-        }
-
-    def make_request(self, query, method='GET'):
-        if method == 'GET':
-            return self.client.get(self.endpoint, data={'query':query}, **self.header).json()
-
-        if method == 'POST':
-            return self.client.post(self.endpoint, data={'query':query}, **self.header).json()
+        create_admin_account()
 
     def create_timetable(self, name, cycle_length, ref_cycle_day, ref_cycle_date, vendors, admin):
         query = '''

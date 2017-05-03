88a490a9a95e0bb8c3e9223c01eda730157f71ea
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-05-03 15:51:22 +0300

    WIP: Fix creation of inactive weekday in timetable

--- app/api/cruds/timetable_crud.py
+++ app/api/cruds/timetable_crud.py
@@ -4,16 +4,18 @@ import graphene
 from graphene_django import DjangoObjectType, DjangoConnectionField
 from django_filters import OrderingFilter, FilterSet
 
-from app.timetables.models import Timetable, VendorService, TimetableManagement
+from app.timetables.models import Timetable, VendorService, TimetableManagement, Vendor, User, Weekday
 from .utils import get_errors, get_object, load_object
 from .vendor_crud import VendorNode
 from .user_crud import UserNode
+from .weekday_crud import WeekdayNode
 
 
 class TimetableFilter(FilterSet):
 
     order_by = OrderingFilter(fields=[('id', 'id'),
                                       ('name', 'name'),
+                                      ('code', 'code'),
                                       ('cycle_length', 'cycle_length'),
                                       ('ref_cycle_length', 'ref_cycle_length'),
                                       ('ref_cycle_date', 'ref_cycle_date'),
@@ -34,7 +36,8 @@ class TimetableFilter(FilterSet):
 class TimetableNode(DjangoObjectType):
     original_id = graphene.Int()
     vendors = DjangoConnectionField(lambda: VendorNode)
-    users = DjangoConnectionField(lambda: UserNode)
+    admins = DjangoConnectionField(lambda: UserNode)
+    inactive_weekdays = DjangoConnectionField(lambda: WeekdayNode)
 
     class Meta:
         model = Timetable
@@ -52,7 +55,7 @@ class CreateTimetable(graphene.relay.ClientIDMutation):
         cycle_length = graphene.Int(required=True)
         ref_cycle_day = graphene.Int(required=True)
         ref_cycle_date = graphene.String(required=True)
-        inactive_weekdays = graphene.Int(required=False)
+        weekday_id = graphene.Int(required=False)
         vendor_id = graphene.Int(required=False)
         is_active = graphene.Boolean(required=False)
         description = graphene.String(required=False)
@@ -74,8 +77,12 @@ class CreateTimetable(graphene.relay.ClientIDMutation):
             timetable.description = args.get('description', '')
             timetable.full_clean()
             timetable.save()
+
+            weekday = Weekday.objects.get(id=args.get('weekday_id'))
+            if weekday:
+                timetable.inactive_weekdays.add(weekday)
             ### TO FIX:
-            ### This part is buggy. Try to save vendor, admin and weekday appropriately
+            ### This part is buggy. Try to save vendor, admin appropriately
             vendor = VendorService.objects.get(id=args.get('vendor_id'))
             if vendor:
                 timetable.vendorservice_set.add(vendor)
@@ -83,7 +90,7 @@ class CreateTimetable(graphene.relay.ClientIDMutation):
             admin = TimetableManagement.objects.get(id=args.get('admin_id'))
             if admin:
                 timetable.timetablemanagement_set.add(admin)
-            # Add implementation for weekday also
+
             return cls(timetable=timetable)
         except ValidationError as e:
             return cls(timetable=None, errors=get_errors(e))

af21288fb4245fc56a0b182331cd4db724e05e62
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-08-15 11:22:31 +0100

    Add description for Userprofile model

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import UserProfile
 
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User Profile', {
+            'fields': ('user', 'custom_auth_id', 'facebook_oauth_id',
+                       'google_oauth_id', 'twitter_oauth_id',),
+            'description': 'This holds extra optional information about admin users.'
+        }),
+    ]

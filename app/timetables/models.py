8d68f9b69f3ebeeb1394ba330e58d64493177425
kola-er <kolawole.erinoso@andela.com>
2017-10-04 09:08:16 +0100

    Prevent servings from being returned for inactive timetable.

--- app/timetables/models.py
+++ app/timetables/models.py
@@ -379,7 +379,7 @@ class ServingAutoUpdate(models.Model):
 
     @classmethod
     def get_servings(cls, timetable, date, vendor=None):
-        if timetable.is_timetable_inactive_this_day(date):
+        if not timetable.is_active or timetable.is_timetable_inactive_this_day(date):
             raise ValidationError(
                 _('Timetable {} is inactive on {}.'.format(
                     timetable.name,

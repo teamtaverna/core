dd03a3d323594c7836525a1be733b689731d98c4
Yogendra Sharma <eh.yogendra@gmail.com>
2017-10-24 13:44:50 +0530

    Change ENVIRONMENT variable from HEROKU to PRODUCTION

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -4,7 +4,7 @@ import dotenv
 dotenv.load()
 
 # Ensure development settings are not used in testing and production:
-if dotenv.get('ENVIRONMENT') == 'HEROKU':
+if dotenv.get('ENVIRONMENT') == 'PRODUCTION':
     from .production import *
 elif dotenv.get('ENVIRONMENT') == 'TRAVIS':
     from .testing import *
